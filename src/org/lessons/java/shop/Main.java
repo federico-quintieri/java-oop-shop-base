3936e0ac73b081d4370b904439dd61a6d6cb76af
Federico Quintieri <fedequinti@gmail.com>
2025-03-17 19:18:43 +0100

    incapsulamento java

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,13 +1,18 @@
 package org.lessons.java.shop;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
 
-        Prodotto merendine = new Prodotto("Kinder Paradiso", "Merendine belle e buone", 2.51, 20);
+        Prodotto merendine = new Prodotto("Kinder Paradiso", "Kinder", BigDecimal.valueOf(2.32),
+                BigDecimal.valueOf(0.2));
 
+        System.out.println(merendine.getCodice());
         merendine.NomeEsteso();
-        System.out.println(merendine.descrizione);
-        merendine.PrezzoBase();
-        merendine.PrezzoIva();
+        System.out.println(merendine.getIva());
+        merendine.setIva(BigDecimal.valueOf(0.40));
+        System.out.println(merendine.getIva());
+
     }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,35 +1,81 @@
 package org.lessons.java.shop;
 
+import java.math.BigDecimal;
 import java.util.Random;
 
 public class Prodotto {
 
     // Attributi classe
-    public int codice;
-    public String nome;
-    public String descrizione;
-    public double prezzo;
-    public int iva;
+    private int codice;
+    private String nome;
+    private String marca;
+    private BigDecimal prezzo;
+    private BigDecimal iva;
 
-    // Costruttore
-    public Prodotto(String nome, String descrizione, double prezzo, int iva) {
+    // Costruttore che popola solo il codice
+    public Prodotto() {
+        Random rand = new Random();
+        this.codice = rand.nextInt();
+    }
+
+    // Costruttore che popola gli attributi
+    public Prodotto(String nome, String marca, BigDecimal prezzo, BigDecimal iva) {
         // Assegno valore random a codice
         Random rand = new Random();
         this.codice = rand.nextInt(); // Genero numero int random e lo assegno ad attributo codice
         // Passo parametri costruttore ad attributi classe
         this.nome = nome;
-        this.descrizione = descrizione;
+        this.marca = marca;
         this.prezzo = prezzo;
         this.iva = iva;
     }
 
-    // Metodi classe
-    public void PrezzoBase() {
-        System.out.println("Prezzo base: " + prezzo);
+    // Metodi getter
+    public int getCodice() {
+        return this.codice;
     }
 
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public BigDecimal getPrezzo() {
+        return this.prezzo;
+    }
+
+    public BigDecimal getIva() {
+        return this.iva;
+    }
+
+    // Metodi setter
+    public void setNome(String nome) {
+        if (nome != null)
+            this.nome = nome;
+    }
+
+    public void setMarca(String marca) {
+        if (marca != null)
+            this.marca = marca;
+    }
+
+    public void setPrezzo(BigDecimal prezzo) {
+        if (prezzo != null)
+            this.prezzo = prezzo;
+    }
+
+    public void setIva(BigDecimal iva) {
+        if (iva != null)
+            this.iva = iva;
+    }
+
+    // Altri metodi
+
     public void PrezzoIva() {
-        double prezzoConIva = this.prezzo + (this.prezzo * this.iva / 100);
+        BigDecimal prezzoConIva = this.prezzo.add(this.prezzo.multiply(this.iva));
         System.out.println("Prezzo con iva: " + prezzoConIva);
     }
 
